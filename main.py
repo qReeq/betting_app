492715ee6c6e9a909c2428fa094832d356054455
qReeq <grzegorzdolatowski1@gmail.com>
2022-11-30 20:19:08 +0100

    LAST STEP LEFT

--- main.py
+++ main.py
@@ -58,6 +58,69 @@ def rebuild_db():
             bet_x = UserBet.create(owner=user, match=x, left_bet=bets[user][x][0], right_bet=bets[user][x][1])
 
 
+def checking_score(choose_out):
+    left_win = False
+    right_win = False
+    draw = False
+    draw_value = 0
+    left_win_value = 0
+    right_win_value = 0
+
+    # CHECKING ACTUAL UPDATED SCORE, LEFT, RIGHT OR DRAW
+    if EndedMatch.get(EndedMatch.name == choose_out.get()).left_score > EndedMatch.get(
+            EndedMatch.name == choose_out.get()).right_score:
+        left_win = True
+        left_win_value = EndedMatch.get(EndedMatch.name == choose_out.get()).left_score + EndedMatch.get(
+            EndedMatch.name == choose_out.get()).right_score
+        print("left")
+    if EndedMatch.get(EndedMatch.name == choose_out.get()).left_score < EndedMatch.get(
+            EndedMatch.name == choose_out.get()).right_score:
+        right_win = True
+        right_win_value = EndedMatch.get(EndedMatch.name == choose_out.get()).left_score + EndedMatch.get(
+            EndedMatch.name == choose_out.get()).right_score
+        print("right")
+    if EndedMatch.get(EndedMatch.name == choose_out.get()).left_score == EndedMatch.get(
+            EndedMatch.name == choose_out.get()).right_score:
+        draw = True
+        draw_value = EndedMatch.get(EndedMatch.name == choose_out.get()).left_score + EndedMatch.get(
+            EndedMatch.name == choose_out.get()).right_score
+        print("draw")
+    # ADDING POINTS
+    for user_bet in UserBet.get(UserBet.match == choose_out.get()).owner:
+        print(user_bet)
+        # if user_bet.left_score > user_bet.right_score == left_win:  # czy trafiona strona, lewa
+        #
+        #     score_for_user = (User
+        #                       .update(score=+1)
+        #                       .where(User.name == user_bet.owner)
+        #                       .execute())
+        #     if user_bet.left_score + user_bet.right_score == left_win_value:  # czy trafiony dokładny wynik
+        #         score_for_user = (User
+        #                           .update(score=+2)
+        #                           .where(User.name == user_bet.owner)
+        #                           .execute())
+        # if user_bet.left_score < user_bet.right_score == right_win:  # czy trafiona strona, prawa
+        #     score_for_user = (User
+        #                       .update(score=+1)
+        #                       .where(User.name == user_bet.owner)
+        #                       .execute())
+        #     if user_bet.left_score + user_bet.right_score == right_win_value:  # czy trafiony dokładny wynik
+        #         score_for_user = (User
+        #                           .update(score=+2)
+        #                           .where(User.name == user_bet.owner)
+        #                           .execute())
+        # if user_bet.left_score == user_bet.right_score == draw:  # czy trafiony remis
+        #     score_for_user = (User
+        #                       .update(score=+1)
+        #                       .where(User.name == user_bet.owner)
+        #                       .execute())
+        #     if user_bet.left_score + user_bet.right_score == draw_value:  # czy trafiony dokładny wynik
+        #         score_for_user = (User
+        #                           .update(score=+2)
+        #                           .where(User.name == user_bet.owner)
+        #                           .execute())
+        #
+
 # rebuild_db()  # IF STH HAPPEN WITH DB, RUN THIS !!!
 
 #
@@ -72,20 +135,27 @@ customtkinter.set_default_color_theme("green")
 root = customtkinter.CTk()
 root.title('Obstawki')
 root.geometry("600x400")
-scoring = ""
-matches = ""
+
 
 # LABELS ENTRY
-for namex in User.select():
-    scoring += namex.name + ": " + str(namex.score) + "\n"
-for matchesx in EndedMatch.select():
-    matches += matchesx.name + ": " + str(matchesx.left_score) + " : " + str(matchesx.right_score) + "\n"
+def score_label_refresh():
+    scoring = ""
+    for namex in User.select():
+        scoring += namex.name + ": " + str(namex.score) + "\n"
+    score_label = customtkinter.CTkLabel(text=scoring, text_font='Tahoma', justify='right')
+    score_label.grid(column=0, row=0, padx=25, pady=25)
 
-score_label = customtkinter.CTkLabel(text=scoring, text_font='Tahoma', justify='right')
-score_label.grid(column=0, row=0, padx=25, pady=25)
 
-matches_label = customtkinter.CTkLabel(text=matches, text_font='Tahoma', justify='right')
-matches_label.grid(column=2, row=0, padx=25, pady=25)
+def match_label_refresh():
+    matches = ""
+    for matchesx in EndedMatch.select():
+        matches += matchesx.name + ": " + str(matchesx.left_score) + " : " + str(matchesx.right_score) + "\n"
+    matches_label = customtkinter.CTkLabel(text=matches, text_font='Tahoma', justify='right')
+    matches_label.grid(column=2, row=0, padx=25, pady=25)
+
+
+match_label_refresh()
+score_label_refresh()
 
 # SHOW MATCHES LIST
 value = ""
@@ -104,7 +174,6 @@ score_input.grid(column=0, row=1, padx=0, pady=25)
 
 
 # BUTTON TO CHANGE THE ACTUAL SCORE IN DB
-# NOT WORKING
 def change_input():
     insert = score_input.get()
     ls = (EndedMatch
@@ -115,8 +184,10 @@ def change_input():
           .update(right_score=insert[2])
           .where(EndedMatch.name == choose.get())
           .execute())
-    print(EndedMatch.get(EndedMatch.name == choose.get()).left_score)
-    print(EndedMatch.get(EndedMatch.name == choose.get()).right_score)
+    match_label_refresh()
+    checking_score(choose_out=choose)
+    score_label_refresh()
+    print(User.get(User.name == "Grzegorz").score)
 
 
 input_button = customtkinter.CTkButton(text='OK', text_font='Tahoma', command=change_input)
